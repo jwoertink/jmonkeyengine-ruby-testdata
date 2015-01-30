f792f830c91630c6b91e92d64782b00e1ee4ed34
Jeremy Woertink <jeremywoertink@gmail.com>
2015-01-30 15:24:42 -0800

    throw to the front of the class path

--- lib/jmonkeyengine/testdata.rb
+++ lib/jmonkeyengine/testdata.rb
@@ -7,7 +7,7 @@ module JMonkeyEngine
 
     def self.included(base)
       jar_path = Pathname(__FILE__).dirname.expand_path.parent.parent + 'vendor'
-      $CLASSPATH << jar_path.to_s
+      $CLASSPATH.unshift(jar_path.to_s)
     end
   end
 end
