8d524639f8367c74d1031f14fad031dd067021f5
Jeremy Woertink <jeremywoertink@gmail.com>
2015-01-30 12:55:02 -0800

    initial structure

---
+++ lib/jmonkeyengine.rb
@@ -0,0 +1,3 @@
+require "jmonkeyengine/testdata/version"
+
+$CLASSPATH << File.join(File.expand_path(File.dirname(__FILE__), '..'), 'vendor', 'lib')
---
+++ lib/jmonkeyengine/testdata/version.rb
@@ -0,0 +1,5 @@
+module JMonkeyEngine
+  module TestData
+    VERSION = "1.0.0"
+  end
+end
