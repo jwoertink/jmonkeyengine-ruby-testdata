023f960fb484d9406ef236afe76a76e8d5bc2af3
Jeremy Woertink <jeremywoertink@gmail.com>
2015-01-30 14:54:51 -0800

    make it shorter

--- lib/jmonkeyengine/testdata.rb
+++ lib/jmonkeyengine/testdata.rb
@@ -6,7 +6,7 @@ module JMonkeyEngine
   module TestData
 
     def self.included(base)
-      jar_path = Pathname.new(__dir__).expand_path.parent + 'vendor' + 'lib'
+      jar_path = Pathname(__dir__).expand_path.parent + 'vendor' + 'lib'
       $CLASSPATH << jar_path
     end
   end
