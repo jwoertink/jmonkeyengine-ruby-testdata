cd5a12718735106f012228ed7564e31520ee759a
Jeremy Woertink <jeremywoertink@gmail.com>
2015-01-30 16:34:08 -0800

    require the jars so they can actually be used

--- lib/jmonkeyengine/testdata.rb
+++ lib/jmonkeyengine/testdata.rb
@@ -8,6 +8,9 @@ module JMonkeyEngine
     def self.included(base)
       jar_path = Pathname(__FILE__).dirname.expand_path.parent.parent + 'vendor'
       $LOAD_PATH.unshift(jar_path.to_s)
+      require 'lib/jME3-testdata.jar'
+      require 'lib/nifty-style-black.jar'
+      require 'lib/nifty-examples.jar'
     end
   end
 end
